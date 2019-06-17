acb7b21f4ac061c1d78fd48f2172db4e1e4ee2d2
Charleese burns <charleeseburns1@yahoo.com>
2019-06-17 02:09:42 +0000

    Automatically backed up by Learn

---
+++ enter_name.rb
@@ -0,0 +1,11 @@
+def get_username
+ puts "Enter your username!"
+ username = gets
+ if username.length < 5
+   puts "Name must be greater than 5 characters!"
+   username = gets
+ else 
+   puts "Hello, " + username
+ end 
+end
+get_username
---
+++ methods.rb
@@ -0,0 +1,19 @@
+def print_students_name
+  puts "safae"
+  puts "danny"
+  puts "courtney"
+  puts "jon"
+end 
+print_students_name
+def greeting_statement
+  puts "Hello World!"
+end  
+greeting_statement
+
+def print_hello_name( name )
+  puts "Hello " + name
+end  
+name = "charleese"
+
+print_hello_name(name)
+ 
